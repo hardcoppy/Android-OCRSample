7b624dbc0226c7dfcca1116fe0644dcc9e452919
Sven <supertypsven@gmail.com>
2021-07-22 22:18:46 +0200

    Intial refactoring of cloned repo.

--- app/src/main/java/org/komamitsu/android_ocrsample/MainActivity.java
+++ app/src/main/java/org/komamitsu/android_ocrsample/MainActivity.java
@@ -1,25 +1,32 @@
 package org.komamitsu.android_ocrsample;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import androidx.exifinterface.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.SparseArray;
-import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
@@ -29,46 +36,32 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_GALLERY = 0;
     private static final int REQUEST_CAMERA = 1;
     private static final int MY_PERMISSIONS_REQUESTS = 0;
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String COLON_SEPARATOR = ":";
+    private static final String IMAGE = "image";
 
     private Uri imageUri;
     private TextView detectedTextView;
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUESTS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                } else {
-                    // FIXME: Handle this case the user denied to grant the permissions
-                }
-                break;
-            }
-            default:
-                // TODO: Take care of this case later
-                break;
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // TODO: Take care of this case later
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSIONS_REQUESTS) {
+            // FIXME: Handle this case the user denied to grant the permissions
         }
     }
 
     private void requestPermissions()
     {
         List<String> requiredPermissions = new ArrayList<>();
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            requiredPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -89,34 +82,21 @@ public class MainActivity extends AppCompatActivity {
 
         requestPermissions();
 
-        findViewById(R.id.choose_from_gallery).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, REQUEST_GALLERY);
-            }
+        findViewById(R.id.take_a_photo).setOnClickListener(v -> {
+            String filename = System.currentTimeMillis() + ".jpg";
+
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, filename);
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+            imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+            Intent intent = new Intent();
+            intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, REQUEST_CAMERA);
         });
 
-        findViewById(R.id.take_a_photo).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String filename = System.currentTimeMillis() + ".jpg";
-
-                ContentValues values = new ContentValues();
-                values.put(MediaStore.Images.Media.TITLE, filename);
-                values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-                imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-                Intent intent = new Intent();
-                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, REQUEST_CAMERA);
-            }
-        });
-
-        detectedTextView = (TextView) findViewById(R.id.detected_text);
+        detectedTextView = findViewById(R.id.detected_text);
         detectedTextView.setMovementMethod(new ScrollingMovementMethod());
     }
 
@@ -137,22 +117,20 @@ public class MainActivity extends AppCompatActivity {
                 TextBlock textBlock = origTextBlocks.valueAt(i);
                 textBlocks.add(textBlock);
             }
-            Collections.sort(textBlocks, new Comparator<TextBlock>() {
-                @Override
-                public int compare(TextBlock o1, TextBlock o2) {
-                    int diffOfTops = o1.getBoundingBox().top - o2.getBoundingBox().top;
-                    int diffOfLefts = o1.getBoundingBox().left - o2.getBoundingBox().left;
-                    if (diffOfTops != 0) {
-                        return diffOfTops;
-                    }
-                    return diffOfLefts;
+            textBlocks.sort((o1, o2) -> {
+                int diffOfTops = o1.getBoundingBox().top - o2.getBoundingBox().top;
+                int diffOfLefts = o1.getBoundingBox().left - o2.getBoundingBox().left;
+                if (diffOfTops != 0) {
+                    return diffOfTops;
                 }
+                return diffOfLefts;
             });
 
             StringBuilder detectedText = new StringBuilder();
             for (TextBlock textBlock : textBlocks) {
-                if (textBlock != null && textBlock.getValue() != null) {
+                if (textBlock != null) {
                     detectedText.append(textBlock.getValue());
+                    //detectedText.append(textBlock.getValue()).append("(").append(textBlock.getCornerPoints()).append(")");
                     detectedText.append("\n");
                 }
             }
@@ -174,6 +152,20 @@ public class MainActivity extends AppCompatActivity {
             options.inSampleSize = 2;
             options.inScreenDensity = DisplayMetrics.DENSITY_LOW;
             bitmap = BitmapFactory.decodeStream(is, null, options);
+            int imgRotation = getImageRotationDegrees(uri);
+
+            int endRotation = (imgRotation < 0) ? -imgRotation : imgRotation;
+            endRotation %= 360;
+            endRotation = 90 * (endRotation / 90);
+            if (endRotation > 0 && bitmap != null) {
+                Matrix m = new Matrix();
+                m.setRotate(endRotation);
+                Bitmap tmp = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true);
+                if (tmp != null) {
+                    bitmap.recycle();
+                    bitmap = tmp;
+                }
+            }
             inspectFromBitmap(bitmap);
         } catch (FileNotFoundException e) {
             Log.w(TAG, "Failed to find the file: " + uri, e);
@@ -193,22 +185,88 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case REQUEST_GALLERY:
-                if (resultCode == RESULT_OK) {
-                    inspect(data.getData());
+        if (requestCode == REQUEST_CAMERA) {
+            if (resultCode == RESULT_OK) {
+                if (imageUri != null) {
+                    inspect(imageUri);
                 }
-                break;
-            case REQUEST_CAMERA:
-                if (resultCode == RESULT_OK) {
-                    if (imageUri != null) {
-                        inspect(imageUri);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public int getImageRotationDegrees(@NonNull Uri imgUri) {
+        int photoRotation = ExifInterface.ORIENTATION_UNDEFINED;
+
+        try {
+            boolean hasRotation = false;
+            //If image comes from the gallery and is not in the folder DCIM (Scheme: content://)
+            String[] projection = {MediaStore.Images.ImageColumns.ORIENTATION};
+            Cursor cursor = this.getContentResolver().query(imgUri, projection, null, null, null);
+            if (cursor != null) {
+                if (cursor.getColumnCount() > 0 && cursor.moveToFirst()) {
+                    photoRotation = cursor.getInt(cursor.getColumnIndex(projection[0]));
+                    hasRotation = photoRotation != 0;
+                    Log.d("Cursor orientation: ", String.valueOf(photoRotation));
+                }
+                cursor.close();
+            }
+
+            //If image comes from the camera (Scheme: file://) or is from the folder DCIM (Scheme: content://)
+            if (!hasRotation) {
+                ExifInterface exif = new ExifInterface(getAbsolutePath(imgUri));
+                int exifRotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_NORMAL);
+                switch (exifRotation) {
+                    case ExifInterface.ORIENTATION_ROTATE_90: {
+                        photoRotation = 90;
+                        break;
+                    }
+                    case ExifInterface.ORIENTATION_ROTATE_180: {
+                        photoRotation = 180;
+                        break;
+                    }
+                    case ExifInterface.ORIENTATION_ROTATE_270: {
+                        photoRotation = 270;
+                        break;
                     }
                 }
-                break;
-            default:
-                super.onActivityResult(requestCode, resultCode, data);
-                break;
+                Log.d(TAG, "Exif orientation: "+ photoRotation);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error determining rotation for image"+ imgUri, e);
         }
+        return photoRotation;
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private String getAbsolutePath(Uri uri) {
+        //Code snippet edited from: http://stackoverflow.com/a/20559418/2235133
+        String filePath = uri.getPath();
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            // Will return "image:x*"
+            String[] wholeID = TextUtils.split(DocumentsContract.getDocumentId(uri), COLON_SEPARATOR);
+            // Split at colon, use second item in the array
+            String type = wholeID[0];
+            if (IMAGE.equalsIgnoreCase(type)) {//If it not type image, it means it comes from a remote location, like Google Photos
+                String id = wholeID[1];
+                String[] column = {MediaStore.Images.Media.DATA};
+                // where id is equal to
+                String sel = MediaStore.Images.Media._ID + "=?";
+                Cursor cursor = this.getContentResolver().
+                        query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                column, sel, new String[]{id}, null);
+                if (cursor != null) {
+                    int columnIndex = cursor.getColumnIndex(column[0]);
+                    if (cursor.moveToFirst()) {
+                        filePath = cursor.getString(columnIndex);
+                    }
+                    cursor.close();
+                }
+                Log.d(TAG, "Fetched absolute path for uri" + uri);
+            }
+        }
+        return filePath;
     }
 }
